0f5b78c1fbb64ff09f4699ae17c1808bc5cad0ba
worace <horace.d.williams@gmail.com>
2015-04-08 10:41:00 -0600

    refactor sessions controller to allow team-auth logic into gem

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,17 +8,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    puts "Sessions#create!"
     auth = request.env["omniauth.auth"]
-    if auth && turing_member?
-      user_info = {github_id: auth.uid, github_token: auth.credentials.token,
-                   github_name: auth.info.nickname, email: auth.info.email}
-      Rails.logger.info("user is turing member, will log them in with info: #{user_info}")
-      @current_user = TuringAuth::User.new(user_info)
+    user_info = {github_id: auth.uid, github_token: auth.credentials.token,
+                 github_name: auth.info.nickname, email: auth.info.email}
+    user = TuringAuth::User.new(user_info)
+    if user.valid? && user.turing_member?
+      @current_user = user
       session[:current_user] = @current_user.as_json
       redirect_to subdomains_path
     else
-      Rails.logger.error("user is not authed or is not turing member...redirect them")
       flash[:error] = "Sorry, only members of the Turing github organization can do that."
       redirect_to root_path
     end
@@ -33,27 +31,5 @@ class SessionsController < ApplicationController
   def destroy
     session[:current_user] = nil
     redirect_to root_path
-  end
-
-  private
-
-  def octokit
-    if request.env["omniauth.auth"] && request.env["omniauth.auth"].credentials.token
-      Octokit::Client.new(:access_token => request.env["omniauth.auth"].credentials.token)
-    else
-      Octokit::Client.new #won't have access to user-specific stuff
-    end
-  end
-
-  def user_gh_teams
-    begin
-      octokit.user_teams.map(&:id)
-    rescue Octokit::NotFound
-      []
-    end
-  end
-
-  def turing_member?
-    (user_gh_teams & TURING_GH_TEAMS).any?
   end
 end
